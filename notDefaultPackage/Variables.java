1997b6e7833da994c9e859cd1dbcdee768785c3c
sagartiwari0608 <sagartiwarisrt@gmail.com>
2024-12-11 00:24:46 +0530

    added another older folder from older learning times

---
+++ notDefaultPackage/Variables.java
@@ -0,0 +1,13 @@
+package notDefaultPackage;
+
+/**
+ * Variables
+ */
+public class Variables {
+
+    public int num = 5;
+    public double decimalNumber = 5.5;
+    char singleCharacter = 'c';
+    public String sentenseOrWord = "hello World! this is a string";
+
+}
